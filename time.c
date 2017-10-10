2066083d66b79cbe302b9ea4b79b03756bc46b53
gaokang <gaokang@embed-tec.com>
2017-10-10 19:36:47 +0800

    修改起始时间为2017年10月10日

--- time.c
+++ time.c
@@ -6,7 +6,7 @@
 #include <windows.h>
 
 
-/*- 起始计算时间是2017-09-20 00:00:00 */
+/*- 起始计算时间是2017-10-10 00:00:00 */
 
 
 static unsigned int calcSec(struct tm *pTm);
@@ -22,7 +22,7 @@ int main(void)
 	time(&timeNow);
 	pTime = localtime(&timeNow);
 	sprintf(&timeStr[0],"%04d-%02d-%02d %02d:%02d:%02d",pTime->tm_year + 1900, pTime->tm_mon+1, pTime->tm_mday, pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
-	printf("\t %s  ( <-- 2017-09-26 00:00:00 )\n\n",timeStr);
+	printf("\t %s  ( <-- 2017-10-10 00:00:00 )\n\n",timeStr);
 	printf("-----------------------------------------------------\n\n");
 
 
@@ -52,8 +52,8 @@ static unsigned int calcSec(struct tm *pTm)
 	unsigned char minVal = 0U;
 	unsigned char secVal = 0U;
 	unsigned char mon_day[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
-	/*- 下面这个数组分别为：9月到9月末、9月到10月末、9月到11月末的天数 */
-	unsigned char day_temp[3] = {5,31+11,30+31+11};
+	/*- 下面这个数组分别为：10月到10月末、10月到11月末的天数、10月到12月末的天数 */
+	unsigned char day_temp[3] = {22,30+22,30+31+22};
 	unsigned char index = 0U;
 
 	if(pTm != NULL)
@@ -68,23 +68,23 @@ static unsigned int calcSec(struct tm *pTm)
 
 		if(yearVal == 2017)
 		{
-			if(monVal == 9)
+			if(monVal == 10)
 			{
-				/*- 当前是2017-09-26 */
-				if(dayVal == 26)
+				/*- 当前是2017-10-10 */
+				if(dayVal == 10)
 				{
-					dayTemp = 0U;
+					dayTemp = 1U;
 				}
-				/*- 是9月，但不是26号 */
+				/*- 是10月，但不是10号 */
 				else
 				{
-					dayTemp = dayVal - 26;
+					dayTemp = dayVal - 10+1;
 				}
 			}
 			else
 			{
-				/*- 不是9月 */
-				dayTemp = day_temp[monVal-9-1] + dayVal;
+				/*- 不是10月 */
+				dayTemp = day_temp[monVal-10-1] + dayVal;
 			}
 		}
 		else
@@ -93,12 +93,12 @@ static unsigned int calcSec(struct tm *pTm)
 			if(monVal == 1)
 			{
 				/*- 是2018年1月 */
-				dayTemp = day_temp[2]+31+dayVal;
+				dayTemp = day_temp[2]+dayVal;
 			}
 			else
 			{
 				/*- 获取2017年的天数和本月的天数之和 */
-				dayTemp = day_temp[2]+31+dayVal;
+				dayTemp = day_temp[2]+dayVal;
 				for(index=0U; index<(monVal-1);index++)
 				{
 					/*- 获取本年本月之前的整月天数 */
